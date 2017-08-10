97b3fd3acd1216a926c216ce94b9532ac67ebbd9
Kristen Kulha and Kelsey Langlois <kelsey@>
2017-08-10 14:51:07 -0700

    Add functionality to form.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("#quiz").submit(function(event) {
+    var scores = [0, 0, 0, 0];
+    var questions = ["question1", "question2", "question3", "question4"];
+
+    questions.forEach(function(q) {
+      var answer = parseInt($("select#" + q).val());
+      scores[answer] += 1;
+    });
+
+    var indexOfMax = scores.indexOf(Math.max.apply(null, scores));
+    $("form").hide();
+    if (indexOfMax === 0) {
+      $(".mr-poopybutthole").show();
+    } else if (indexOfMax === 1) {
+      $(".birdperson").show();
+    } else if (indexOfMax === 2) {
+      $(".summer").show();
+    } else {
+      $(".mr-meeseeks").show();
+    }
+
+    event.preventDefault();
+  });
+});
